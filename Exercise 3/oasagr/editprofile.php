4cb19afb20978a0d56b3defdf6c05c672e08831e
kkoukakis <kkoukakis@windowslive.com>
2020-01-11 11:43:22 +0200

    moved editprofile.php

---
+++ Exercise 3/oasagr/editprofile.php
@@ -0,0 +1,31 @@
+<body>
+<p>
+Edit Profile
+</p>
+
+	<p>
+		Όνομα: 
+		<input type="text" name="firstname">
+	</p>
+	
+	<p>
+		Επίθετο:<input type="text" name="lastname">
+	</p>
+	<p>
+		Email:<input type="text" name="email">
+	</p>
+	<p>
+		Τηλέφωνο:<input type="text" name="phonenumber">
+	</p>
+	<p>
+		Τύπος Εισιτηρίου:<input type="text" name="ticket">
+	</p>
+	
+	<p>
+		Κωδικός:<input type="text" name="password">
+	</p>
+	<p>
+		Επαλήθευση Κωδικού:<input type="text" name="password">
+	</p>
+	<button type="button">Aλλλαγή</button> 
+</body>
