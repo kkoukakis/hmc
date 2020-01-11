300c7339522b588f0f02b6e73a58372f509c06b1
kkoukakis <kkoukakis@windowslive.com>
2020-01-11 12:00:48 +0200

    fixes on bootstrap and css

--- Exercise 3/oasagr/components/navbar/navbar.php
+++ Exercise 3/oasagr/components/navbar/navbar.php
@@ -7,10 +7,9 @@
      <li><a  href="/FAQ"      class="">Συχνές Ερωτήσεις</a></li>
 	</ul>
 	<ul class="nav navbar-nav navbar-right">
-     <li><input id="search" type="text" placeholder="Search..." height="20"/><img src="components\navbar\English-Language-Flag-1-icon.png" title="English" height="20" width="20"></li>
-	<li><a href="index.html"  class=""> <img src="components\navbar\English-Language-Flag-1-icon.png" title="English" height="20" width="17"> <br> </a></li>
-     <li><a href="index.html" class=""> <img src="components\navbar\login.png" title="login" height="20" width="17"> <br> </a></li>
-	<li><a href="login.php"    class="">Σύνδεση</a></li>
+     <li><input id="search" style="margin-top: 12px;"type="text" placeholder="Search..." height="20"/><span class="oi oi-magnifying-glass"></span></li>
+	<li><a href="index.html"  class=""> <img src="components\navbar\English-Language-Flag-1-icon.png" title="English" height="20" width="20"> <br> </a></li>
+     <li><a href="login.php"  class=""> <img src="components\navbar\login.png" title="login" height="20" width="17"> Σύνδεση<br> </a></li>
 	<li><a href="register.php" class="">Εγγραφή</a></li>
 	</ul>
  </div>
