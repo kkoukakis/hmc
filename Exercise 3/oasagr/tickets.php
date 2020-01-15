5c1a9fdbcadd76e741dfc4665e220f6ba7f3c843
kkoukakis <kkoukakis@windowslive.com>
2020-01-15 22:36:21 +0200

    buy

--- Exercise 3/oasagr/tickets.php
+++ Exercise 3/oasagr/tickets.php
@@ -10,32 +10,32 @@ $breadcrumb = array('Oasa.gr','Εισιτήρια','Αγορά');
 $breadlinks = array('index.php','#','tickets.php');
 include('components\breadcrumbs\breadcrumb.php');
 ?>
+<?php
 
-<?php 
-
+$price = 1.40;
 //KANONIKO
-if(isset($_SESSION['type']) &&($_SESSION['type']== 4 )){
+if(isset($_SESSION['type']) &&($_SESSION['type']== 1 )){
 
 
-
+$price = 1.40;
 
 
 }
 //MEIWMENO
-if(isset($_SESSION['type']) &&($_SESSION['type']==4)){
+if(isset($_SESSION['type']) &&($_SESSION['type']==2)){
 
 
 
-
+  $price = 0.6;
 
 }
 
 //DWREAN
-if(isset($_SESSION['type']) && ($_SESSION['type']==4)){
+if(isset($_SESSION['type']) && ($_SESSION['type']==3)){
 
 
 
-
+  $price = 0;
 
 }
 
@@ -43,12 +43,27 @@ if(isset($_SESSION['type']) && ($_SESSION['type']==4)){
 if(isset($_SESSION['type']) && ($_SESSION['type']==4)){
 
 
-
+  $price = 0.6;
 
 
 }
 ?>
-<?php
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  if(!isset($_POST['tick'])){
+ echo('<script>alert(Αγοράσατε '. $_POST['num'] . ' εισιτηριο(α) των: "'. $_POST['num'] * $price . 'Ευρώ");</script>');
+  }else{
+    
+    echo('<script>alert("Φορτίσατε το εισιτηριο σας με '. $_POST['num'] . ' εισιτήρια των '. $_POST['time'] .' και συνολικά κόστισε: '. $_POST['num'] * $price .'Ευρώ");</script>');
+  }
+
+}
+
+
+
+
 if(!isset($_SESSION['type']))
 {
   echo('
@@ -56,10 +71,21 @@ if(!isset($_SESSION['type']))
   <h2>ΑΓΟΡΑ/ΦΟΡΤΙΣΗ ΕΙΣΙΤΗΡΙΟΥ </h2>
   <form method="post">
     <p>Ποσότητα εισιτηρίων</p>
-    <select name="num" id="num" type="text" title="Πόσα εισιτήρια θα πάρετε" placeholder="1" />
+    <select name="num" id="num" name="num" class="browser-default">
+    <option value="1"  title="Για τους Κοινούς Επιβάτες, Τουρίστες" >1</option>
+    <option value="2" title="Για τους Φοιτητές" >2</option>
+    <option value="3" title="Για τους Άνεργους,Aμέα" >5</option>  
+    <option value="3" title="Για τους Άνεργους,Aμέα" >10</option>  
+    </select>
     <p>Είδος εισιτηρίων</p>
-    <input name="password" id="password" type="password" title="password" placeholder="password" />
-    <button type="submit" class="btn">Αγορά</button>
+    <select name="time" id="time" name="time" class="browser-default">
+    <option value="90λεπτων"  title="90λεπτα της ώρας" >90λεπτων</option>
+    </select>
+    <p>Αριθμός Κάρτας</p>
+    <input name="card" id="card" type="text" title="password" placeholder="" />
+    <p>Αριθμός Εισιτηρίου</p>
+    <input name="tick" id="tick" type="text" title="tick" placeholder="Αριθμός Εισιτηρίου" />
+    <button type="submit" class="btn">Αγορά / Φορτιση</button>
     <a class="forgot" href="login.php">Δικαιούστε μειωμένο κάντε σύνδεση?</a>
   </form>
 </div>
